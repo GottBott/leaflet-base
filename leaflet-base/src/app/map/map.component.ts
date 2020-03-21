6f4be499b49c73ec476b35f19bd3a12f52a82db7
Ben Gotthold <bengotthold@gmail.com>
2020-03-21 07:55:41 -0600

    minimum setup for leaflet in angular

---
+++ leaflet-base/src/app/map/map.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  public map: L.map
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.map = L.map('map', {
+      center: [39.8282, -105.5795],
+      zoom: 8
+    })
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map)
+
+
+
+  }
+
+}
